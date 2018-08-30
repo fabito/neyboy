f7060466faa6fbab59b2e31ebc7961f4c2554f1f
fabito <fuechi@ciandt.com>
2018-08-30 01:36:42 -0300

    upgrade to latest baselines and cleanup

--- neyboy/baselines/cmd_util.py
+++ neyboy/baselines/cmd_util.py
@@ -6,6 +6,7 @@ from baselines.bench import Monitor
 from baselines.common import set_global_seeds
 from baselines.common.atari_wrappers import MaxAndSkipEnv, WarpFrame
 from baselines.common.cmd_util import arg_parser
+from baselines.common.vec_env.dummy_vec_env import DummyVecEnv
 from baselines.common.vec_env.subproc_vec_env import SubprocVecEnv
 from gym import wrappers
 
@@ -13,17 +14,19 @@ import gym_neyboy
 from neyboy.baselines.neyboy_wrappers import Cropper
 
 
-def make_neyboy_environment(env_id, seed=0, rank=0, allow_early_resets=False, frame_skip=4, save_obs=False):
+def make_neyboy_environment(env_id, seed=0, rank=0, allow_early_resets=False, frame_skip=4, save_video=False):
     env = gym.make(env_id)
     env = MaxAndSkipEnv(env, skip=frame_skip)
     env.seed(seed + rank)
     logdir = logger.get_dir() and os.path.join(logger.get_dir(), str(rank))
-    # env = wrappers.Monitor(env, logdir, force=True)
-    env = Monitor(env, logdir, allow_early_resets=allow_early_resets)
+    if save_video:
+        env = wrappers.Monitor(env, logdir, force=True)
+    else:
+        env = Monitor(env, logdir, allow_early_resets=allow_early_resets)
     return env
 
 
-def make_neyboy_env(env_id, num_env, seed, wrapper_kwargs=None, start_index=0, allow_early_resets=False, frame_skip=4, save_obs=False):
+def make_neyboy_env(env_id, num_env, seed, wrapper_kwargs=None, start_index=0, allow_early_resets=False, frame_skip=4, save_video=False):
     """
     Create a wrapped, monitored SubprocVecEnv for Neyboy.
     """
@@ -32,19 +35,21 @@ def make_neyboy_env(env_id, num_env, seed, wrapper_kwargs=None, start_index=0, a
 
     def make_env(rank):
         def _thunk():
-            env = make_neyboy_environment(env_id, seed, rank, allow_early_resets, frame_skip=frame_skip, save_obs=save_obs)
-            env = Cropper(env)
+            env = make_neyboy_environment(env_id, seed, rank, allow_early_resets, frame_skip=frame_skip, save_video=save_video)
+            # env = Cropper(env)
             env = WarpFrame(env)
             return env
         return _thunk
 
     set_global_seeds(seed)
-    return SubprocVecEnv([make_env(i + start_index) for i in range(num_env)])
 
+    envs = [make_env(i + start_index) for i in range(num_env)]
+    if num_env > 1:
+        env = SubprocVecEnv(envs)
+    else:
+        env = DummyVecEnv(envs)
 
-def wrap_neyboy_dqn(env):
-    from baselines.common.atari_wrappers import wrap_deepmind
-    return wrap_deepmind(env, episode_life=False, clip_rewards=False, frame_stack=True, scale=True)
+    return env
 
 
 def neyboy_arg_parser():
--- neyboy/baselines/enjoy.py
+++ neyboy/baselines/enjoy.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3
-import multiprocessing
-import sys
-from time import sleep
-
-import tensorflow as tf
 from baselines import logger
+from baselines.common.policies import build_policy
 from baselines.common.vec_env.vec_frame_stack import VecFrameStack
-from baselines.ppo2.policies import CnnPolicy, LstmPolicy, LnLstmPolicy, MlpPolicy
 from baselines.ppo2.ppo2 import Model
+from gym import logger as gym_logger
 
 from neyboy.baselines.cmd_util import neyboy_arg_parser, make_neyboy_env
 
@@ -15,20 +11,10 @@ from gym import wrappers
 
 
 def train(env_id, seed, policy, load_path, num_episodes, frame_skip, no_render):
-    ncpu = multiprocessing.cpu_count()
-    if sys.platform == 'darwin':
-        ncpu //= 2
-    config = tf.ConfigProto(allow_soft_placement=True,
-                            intra_op_parallelism_threads=ncpu,
-                            inter_op_parallelism_threads=ncpu)
-    config.gpu_options.allow_growth = True  # pylint: disable=E1101
-    tf.Session(config=config).__enter__()
 
-    env = make_neyboy_env(env_id, 1, seed, allow_early_resets=True, frame_skip=frame_skip, save_obs=True)
+    env = make_neyboy_env(env_id, 1, seed, allow_early_resets=True, frame_skip=frame_skip, save_video=True)
     env = VecFrameStack(env, 4)
-
-
-    policy = {'cnn': CnnPolicy, 'lstm': LstmPolicy, 'lnlstm': LnLstmPolicy, 'mlp': MlpPolicy}[policy]
+    policy = build_policy(env, policy)
     ob_space = env.observation_space
     ac_space = env.action_space
     ent_coef = .01
@@ -39,16 +25,24 @@ def train(env_id, seed, policy, load_path, num_episodes, frame_skip, no_render):
                   nsteps=0, ent_coef=ent_coef, vf_coef=vf_coef,
                   max_grad_norm=max_grad_norm)
     model.load(load_path)
+
     for _ in range(num_episodes):
-        observation = env.reset()
-        done = False
+        if not no_render:
+            env.render()
+        observation, done = env.reset(), False
+        if not no_render:
+            env.render()
+        episode_rew = 0
+        score = 0
         while not done:
             if not no_render:
                 env.render()
             action, _, _, _ = model.step(observation)
             observation, reward, done, info = env.step(action)
-        print(reward, done, info)
-        sleep(1)
+            episode_rew += reward
+            score = info[0]
+        print('Episode reward={}, info={}'.format(episode_rew, score))
+
 
 
 def main():
@@ -60,6 +54,7 @@ def main():
     parser.add_argument('--no-render', default=False, action='store_true')
     args = parser.parse_args()
     logger.configure()
+    # gym_logger.set_level(gym_logger.DEBUG)
     train(args.env, seed=args.seed, policy=args.policy, load_path=args.load_path, num_episodes=args.num_episodes,
           frame_skip=args.frame_skip, no_render=args.no_render)
 
