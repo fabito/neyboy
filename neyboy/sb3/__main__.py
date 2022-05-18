1b07b428aff67be5c9b3cc2307781beaa8117126
Fabio Uechi <308613+fabito@users.noreply.github.com>
2022-05-18 12:17:13 +1200

    record score and hiscore

--- neyboy/sb3/__main__.py
+++ neyboy/sb3/__main__.py
@@ -1,13 +1,56 @@
 import datetime
 import os
+from collections import defaultdict
 
 from stable_baselines3 import PPO
 from stable_baselines3.common.callbacks import CheckpointCallback, BaseCallback, CallbackList, EvalCallback
-from stable_baselines3.common.logger import configure, Image
+from stable_baselines3.common.logger import configure, Image, TensorBoardOutputFormat
+from stable_baselines3.common.utils import safe_mean
 from stable_baselines3.common.vec_env import VecFrameStack, VecTransposeImage
 from stable_baselines3.common.env_checker import check_env
 
 from neyboy.sb3.cmd_util import neyboy_arg_parser, make_neyboy_env
+
+
+class RawStatisticsCallback(BaseCallback):
+    """
+    Callback used for logging raw episode data (return and episode length).
+    """
+
+    def __init__(self, verbose=0):
+        super().__init__(verbose)
+        # Custom counter to reports stats
+        # (and avoid reporting multiple values for the same step)
+        self._timesteps_counter = 0
+        self._iterations_counter = 0
+        self._tensorboard_writer = None
+        self._rollout_info_buffer = defaultdict(list)
+
+    def _init_callback(self) -> None:
+        # Retrieve tensorboard writer to not flood the logger output
+        for out_format in self.logger.output_formats:
+            if isinstance(out_format, TensorBoardOutputFormat):
+                self._tensorboard_writer = out_format
+        assert self._tensorboard_writer is not None, "You must activate tensorboard logging when using RawStatisticsCallback"
+
+    def _on_step(self) -> bool:
+        for info in self.locals["infos"]:
+            if all(k in info for k in ("score", "hiscore")):
+                self._rollout_info_buffer["score"].append(info["score"])
+                self._rollout_info_buffer["hiscore"].append(info["hiscore"])
+        return True
+
+    def _on_rollout_start(self) -> None:
+        self._rollout_info_buffer = defaultdict(list)
+
+    def _on_rollout_end(self) -> None:
+        # Display training infos
+        log_interval = self.locals['log_interval']
+        iteration = self.locals['iteration']
+        if log_interval is not None and iteration % log_interval == 0:
+            if len(self._rollout_info_buffer) > 0:
+                self.logger.record("rollout/ep_score_mean", safe_mean(self._rollout_info_buffer["score"]))
+                self.logger.record("rollout/ep_hiscore_mean", safe_mean(self._rollout_info_buffer["hiscore"]))
 
 
 class ImageRecorderCallback(BaseCallback):
@@ -35,6 +78,7 @@ def main():
     parser.add_argument('--batch-size', type=int, default=64)
     parser.add_argument('--num-epoch', type=int, default=10)
     parser.add_argument('--lr', type=float, default=3e-4)
+    parser.add_argument('--clip-range', type=float, default=0.2)
     parser.add_argument('--checkpoint-save-freq', type=int, default=1000)
     parser.add_argument('--load-path', help='load path', default=None)
     parser.add_argument('-v', '--verbose', action='store_true')
@@ -64,7 +108,7 @@ def main():
                                              name_prefix='rl_model')
 
     # Create the callback list
-    callbacks = [checkpoint_callback, ImageRecorderCallback()]
+    callbacks = [checkpoint_callback, ImageRecorderCallback(), RawStatisticsCallback()]
 
     env = VecTransposeImage(VecFrameStack(_env, 4))
     model = PPO(policy=args.policy,
@@ -72,12 +116,12 @@ def main():
                 n_steps=args.n_steps,
                 verbose=verbose,
                 batch_size=args.batch_size,
-                clip_range=lambda f: f * 0.1,
+                clip_range=args.clip_range,
                 n_epochs=args.num_epoch,
                 learning_rate=args.lr,
                 tensorboard_log=logger.get_dir(),
                 )
-    model.learn(total_timesteps=total_timesteps, callback=callbacks)
+    model.learn(total_timesteps=total_timesteps, callback=callbacks, log_interval=1)
 
 
 if __name__ == '__main__':
